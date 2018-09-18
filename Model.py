45a6be7175359deab1b6be54638f0dc08c86b29d
Qian <161250102@smail.nju.edu.cn>
2018-09-18 18:33:35 +0800

    处理html网页中的历史消息列表

--- Model.py
+++ Model.py
@@ -4,7 +4,30 @@
 class Account(object):
 
     def __init__(self):
-        self.biz = ''
-        self.nickname = ''
-        self.description = ''
-        self.head_image = ''
+        self.biz = ''  # 公众号id
+        self.nickname = ''  # 公众号名称
+        self.description = ''  # 公众号描述
+        self.head_image = ''  # 公众号头像
+        self.weixin_id = None  # 公众号个性id
+        self.updated_time = None  # 更新时间
+
+
+class Msg(object):
+
+    def __int__(self):
+        self.title = ''  # 文章标题
+        self.digest = ''  # 文章摘要
+        self.author = ''  # 作者
+        self.publish_time = ''  # 发布时间
+        self.read_num = None  # 阅读量
+        self.like_num = None  # 点赞量
+        self.reward_num = None  # 赞赏量
+        self.idx = 0  # 消息序号
+        self.biz = ''  # 公众号id
+        self.mid = ''  # 消息id
+        self.sn = ''  # 随机加密字符串，对于每条消息是唯一的
+        self.source_url = ''  # 阅读原文链接地址
+        self.cover = ''  # 封面图片地址
+        self.delete_flag = 0  # 是否被删除 0-已被删除
+        self.copyright_stat = '',  # 版权信息
+        self.updated_time = ''  # 记录更新时间
--- Rules.py
+++ Rules.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import re
 import json
+import datetime
 from mitmproxy import http
 from mitmproxy import ctx
 from Data import *
@@ -28,15 +29,28 @@ class Rules(object):
     # 处理历史消息列表（html格式）
     def history_html(self, flow: http.HTTPFlow) -> None:
         text = flow.response.text
+        # 公众号信息处理
         account = Model.Account()
         account.biz = re.search(r'var __biz = "(.+)";', text, re.M).group(1)
         account.nickname = re.search(r'var nickname = "(.+)" ', text, re.M).group(1)
         account.description = re.search(r'<p class="profile_desc">(\s*)(.*)(\s*)</p>', text, re.M).group(2).strip()
         account.head_image = re.search(r'var headimg = "(.+)" ', text, re.M).group(1)
+        account.updated_time = datetime.datetime.now()
         self._data_service.save_account(account)
+        # 历史消息处理
         msg_list = re.search(r"var msgList = '(.+)';", text, re.M).group(1)
         msg_list = self.remove_escapes(msg_list)
         json_object = json.loads(msg_list, encoding='utf-8')
+        for obj in json_object["list"]:
+            time_stamp = obj["comm_msg_info"]["datetime"]
+            # 处理主图文消息
+            app_msg = obj["app_msg_ext_info"]
+            if app_msg.has_keys('del_flag') and app_msg["del_flag"] == 1:
+                msg = self.parse_article(app_msg, time_stamp)
+            if app_msg["is_multi"] == 1:
+                for item in app_msg["multi_app_msg_item_list"]:
+                    msg = self.parse_article(item, time_stamp)
+
         scroll_js = '''
         <script type="text/javascript">
             var end = document.createElement("p");
@@ -68,3 +82,22 @@ class Rules(object):
         s = re.sub(r'&(lt|gt|nbsp|amp|quot);', lambda matched: replace_dict[matched.group(1)], s)
         s = s.replace(r'\"', '"').replace(r'\\/', '/')
         return s
+
+    @staticmethod
+    def parse_article(app_msg, time_stamp):
+        msg = Model.Msg()
+        msg.title = app_msg["title"]
+        msg.digest = app_msg["digest"]
+        msg.author = app_msg["author"]
+        msg.publish_time = time_stamp
+        url = app_msg["content_url"]
+        msg.idx = re.search(r'idx=([0-9]+)', url).group(1)
+        msg.biz = re.search(r'__biz=([a-zA-Z0-9|=]+)', url).group(1)
+        msg.mid = re.search(r'mid=([0-9]+)', url).group(1)
+        msg.sn = re.search(r'sn=([a-z0-9]+)', url).group(1)
+        msg.source_url = app_msg["source_url"]
+        msg.cover = app_msg["cover"]
+        msg.delete_flag = app_msg["del_flag"]
+        msg.copyright_stat = app_msg["copyright_stat"]
+        msg.updated_time = datetime.datetime.now()
+        return msg
